e6535177a391d80ffbc47941ea8dfb7bfb0e6d64
Jhon Valderrama <jhonvalderramaa7@gmail.com>
2025-07-03 21:57:28 -0500

    fix screen

--- app/commons/time.py
+++ app/commons/time.py
@@ -12,10 +12,13 @@ def get_posix_time_until_day() -> int:
     colombia_tz = timezone(timedelta(hours=-5))
 
     # Get the current time in the Colombian time zone
-    current_time = datetime.now(colombia_tz)
+    current_time = datetime(hour=21,year=2025,month=7,day=2)
 
     # Create a new datetime with hours, minutes, and seconds set to zero
     day_time = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
 
     # Convert the datetime back to a POSIX timestamp
     return int(day_time.timestamp())
+
+if __name__ == "__main__":
+    print(get_posix_time_until_day())
